0802eb747f721eb28341855cc592750df701cb6d
qbibubi <jakub.sobieraj2137@gmail.com>
2023-12-17 14:34:26 +0100

    movement working, rendering some tiles

---
+++ scripts/Player.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+
+public partial class Player : Area2D
+{
+	[Export] public int movementSpeed = 120;
+	public Vector2 screenSize;
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		screenSize = GetViewportRect().Size;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		var animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		var velocity = Vector2.Zero;
+
+		if (Input.IsActionPressed("moveUp")) { velocity.Y -= 1; }
+		if (Input.IsActionPressed("moveDown")) { velocity.Y += 1; }
+		if (Input.IsActionPressed("moveLeft")) { velocity.X -= 1; }
+		if (Input.IsActionPressed("moveRight")) { velocity.X += 1; }
+
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * movementSpeed;
+			animatedSprite.Play();
+		}
+		else
+		{
+			animatedSprite.Stop();
+		}
+
+		if (velocity.X < 0)
+		{
+			animatedSprite.FlipH = true;
+		}
+		else if (velocity.X > 0)
+		{
+			animatedSprite.FlipH = false;
+		}
+
+		Position += velocity * (float)delta;
+	}
+}
